067c76ff7a044ef3b81fb8c0884aceb83ecdeec0
hoshico <melancholinista.yasu@gmail.com>
2021-05-02 22:39:48 +0900

    セレクトタブ作成

---
+++ app/javascript/ingredients.js
@@ -0,0 +1,24 @@
+const pfc = function () {
+   
+   const selectMeet = document.getElementById("meet");
+    selectMeet.addEventListener('change', function (){
+      const meetSelectBox = document.getElementById("meet");
+      const dataNumP = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-protein");
+      const dataNumF = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-fat");
+      const dataNumC = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-carbo");
+      console.log(dataNumP);
+      console.log(dataNumF);
+      console.log(dataNumC);
+
+      const quantityInput = document.getElementById("quan-input");
+      quantityInput.addEventListener("input", () => {
+         const inputValue = quantityInput.value;
+         console.log(inputValue * dateNumP);
+         console.log(inputValue * dateNumF);
+         console.log(inputValue * dateNumC);
+      })   
+  })
+};
+ 
+window.addEventListener("load", pfc);
+ 
--- app/models/meet.rb
+++ app/models/meet.rb
@@ -1,15 +1,15 @@
 class Meet < ActiveHash::Base
   self.data = [
       { id: 1, name: '--' },
-      { id: 2, name: '鶏ムネ(皮あり)', multiple_p:0.19, multiple_f:0.1, multiple_c: 0 },
-      { id: 3, name: '鶏ムネ(皮なし)', multiple_p:0.22, multiple_f:0.02, multiple_c: 0 },
-      { id: 4, name: 'ささみ', multiple_p:0.23, multiple_f:0, multiple_c: 0 },
-      { id: 5, name: '鶏もも', multiple_p:0.16, multiple_f:0.14, multiple_c: 0 },
-      { id: 6, name: '牛モモ', multiple_p:0.2, multiple_f:0.04, multiple_c: 0  },
-      { id: 7, name: '牛バラ', multiple_p:0.14, multiple_f:0.32, multiple_c: 0  },
+      { id: 2, name: '鶏ムネ(皮あり)', protein: 0.19, fat: 0.1, carbo: 0 },
+      { id: 3, name: '鶏ムネ(皮なし)', protein: 0.22, fat: 0.02, carbo: 0 },
+      { id: 4, name: 'ささみ', protein: 0.23, fat: 0, carbo: 0 },
+      { id: 5, name: '鶏もも', protein: 0.16, fat: 0.14, carbo: 0 },
+      { id: 6, name: '牛モモ', protein: 0.2, fat: 0.04, carbo: 0  },
+      { id: 7, name: '牛バラ', protein: 0.14, fat: 0.32, carbo: 0  },
    ]
 
       include ActiveHash::Associations
       has_many :menus
 
-   end
+end
