04160525cfd1f5ffbd30b20ca20c8fde1f0173ba
hoshico <melancholinista.yasu@gmail.com>
2021-05-03 09:47:48 +0900

    Update ingredients.js

--- app/javascript/ingredients.js
+++ app/javascript/ingredients.js
@@ -6,16 +6,16 @@ const pfc = function () {
       const dataNumP = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-protein");
       const dataNumF = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-fat");
       const dataNumC = meetSelectBox.options[ meetSelectBox.selectedIndex].getAttribute("data-carbo");
-      console.log(dataNumP);
-      console.log(dataNumF);
-      console.log(dataNumC);
+      // console.log(dataNumP);
+      // console.log(dataNumF);
+      // console.log(dataNumC);
 
       const quantityInput = document.getElementById("quan-input");
       quantityInput.addEventListener("input", () => {
          const inputValue = quantityInput.value;
-         console.log(inputValue * dateNumP);
-         console.log(inputValue * dateNumF);
-         console.log(inputValue * dateNumC);
+         console.log(inputValue * dataNumP);
+         console.log(inputValue * dataNumF);
+         console.log(inputValue * dataNumC);
       })   
   })
 };
