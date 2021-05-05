fea18dd2d4bc9fde54ad691eb089917ab606b401
hoshico <melancholinista.yasu@gmail.com>
2021-05-05 17:59:53 +0900

    マイグレーションファイル

--- db/migrate/20210427102207_create_menus.rb
+++ db/migrate/20210427102207_create_menus.rb
@@ -3,9 +3,8 @@ class CreateMenus < ActiveRecord::Migration[6.0]
     create_table :menus do |t|
  
       t.string  :title,      null: false
-      t.text    :recipe,     null: false
-      t.integer :meet_id     
-      t.references :user, foreign_key: true
+      t.text    :recipe,     null: false   
+      t.references :user,    null: false, foreign_key: true
       t.timestamps
     end
   end
