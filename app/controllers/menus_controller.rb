47d2966134b2cc273a14dc9f0e28ed6c08f6c3f5
hoshico <melancholinista.yasu@gmail.com>
2021-05-06 00:44:53 +0900

    修正

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,28 +1,32 @@
 class MenusController < ApplicationController
-  before_action :set_menu, only: [:show, :edit, :update, :destroy]
+  # before_action :set_menu, only: [:show, :edit, :update, :destroy]
 
   def index
     @menus = Menu.all.order("created_at DESC")
   end 
 
   def new
-    @menu = Menu.new
     @menu_food_stuff = MenuFoodStuff.new
   end
 
   def create
     @menu_food_stuff = MenuFoodStuff.new(menu_params)
-    Menu.create(menu_params)
+    if @menu_food_stuff.valid?
+      @menu_food_stuff.save
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+    @menu_food_stuff = MenuFoodStuff.find(params[:menu_id])
   end
 
   def update
-    if @menu.update(menu_params)
+    if @menu_food_stuff.update(menu_params)
       redirect_to menu_path(@menu.id)
     else
       render :edit
@@ -30,16 +34,16 @@ class MenusController < ApplicationController
   end
 
   def destroy
-    @menu.destroy
+    @menu_food_stuff.destroy
     redirect_to root_path
   end
 
   private
   def menu_params
-    params.require(:menu).permit(:title, :image, :recipe, :meet_id, :meet_quantity, :fish_id, :fish_quantity, :vege_id, :vege_quantity, :dairy_id, :dairy_quantity, :etc_food, :total_p, :total_f, :total_c).merge(user_id: current_user.id, menu_id: params[:menu_id])
+    params.require(:menu_food_stuff).permit(:title, :image, :recipe, :meet_id, :meet_quantity, :fish_id, :fish_quantity, :vege_id, :vege_quantity, :dairy_id, :dairy_quantity, :etc_food, :total_p, :total_f, :total_c).merge(user_id: current_user.id)
   end
 
-  def set_menu
+  def load_menu
     @menu = Menu.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'menus#index'
-  resources :menus
+  resources :menus 
   resources :users, only: :show
 end
