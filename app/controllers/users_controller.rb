f40e051be5cb62294ab5fd1c58f5d5b0b04ec424
hoshico <melancholinista.yasu@gmail.com>
2021-05-08 18:10:20 +0900

    レシピ一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @menus = @user.menus
+   
+    # @foodstuff = @menu.food_stuff
+    # protein = @foodstuffs.sum(:total_p)
+    # fat = @foodstuffs.sum(:total_f)
+    # carbo = @foodstuffs.sum(:total_c)
+    # @mypfc_chart = @pfc_chart = {'タンパク質' => protein, '脂質' => fat, '炭水化物' => carbo }
   end
 end
