013e73b81b529359341c378fe9fe8ea5fd586535
hoshico <melancholinista.yasu@gmail.com>
2021-05-09 12:25:33 +0900

    修正

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -5,8 +5,8 @@ class Fish < ActiveHash::Base
           { id: 3, name: 'たら', protein: 0.22, fat: 0.02, carbo: 0 },
           { id: 4, name: 'たこ', protein: 0.23, fat: 0, carbo: 0 },
           { id: 5, name: 'いか', protein: 0.16, fat: 0.14, carbo: 0 },
-          { id: 6, name: '牛モモ', protein: 0.2, fat: 0.04, carbo: 0  },
-          { id: 7, name: '牛バラ', protein: 0.14, fat: 0.32, carbo: 0  },
+          { id: 6, name: 'サバ', protein: 0.2, fat: 0.04, carbo: 0  },
+          { id: 7, name: 'タイ', protein: 0.14, fat: 0.32, carbo: 0  },
        ]
     
       include ActiveHash::Associations
