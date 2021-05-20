d41f8d79d45a713411299974a76b35ec67be25fa
hoshico <melancholinista.yasu@gmail.com>
2021-05-20 17:07:09 +0900

    修正

--- app/models/meet.rb
+++ app/models/meet.rb
@@ -5,10 +5,10 @@ class Meet < ActiveHash::Base
       { id: 3, name: '鶏ムネ(皮なし)', protein: 0.22, fat: 0.02, carbo: 0 },
       { id: 4, name: 'ささみ', protein: 0.23, fat: 0, carbo: 0 },
       { id: 5, name: '鶏もも', protein: 0.16, fat: 0.14, carbo: 0 },
-      { id: 6, name: '牛モモ', protein: 0.2, fat: 0.04, carbo: 0  },
-      { id: 7, name: '牛バラ', protein: 0.14, fat: 0.32, carbo: 0  },
-      { id: 8, name: '豚ロース', protein: 0.19, fat: 0.19, carbo: 0  },
-      { id: 9, name: '豚もも', protein: 0.20, fat: 0.1, carbo: 0  },
+      { id: 6, name: '牛モモ', protein: 0.2, fat: 0.04, carbo: 0 },
+      { id: 7, name: '牛バラ', protein: 0.14, fat: 0.32, carbo: 0 },
+      { id: 8, name: '豚ロース', protein: 0.19, fat: 0.19, carbo: 0 },
+      { id: 9, name: '豚もも', protein: 0.20, fat: 0.1, carbo: 0 },
    ]
 
       include ActiveHash::Associations
